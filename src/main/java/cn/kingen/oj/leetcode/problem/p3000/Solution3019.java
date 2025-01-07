88aadadf7bfa2f9d4dfab8bf62bb33055ebbac37
kingen <wsg787@126.com>
2025-01-07 09:19:47 +0800

    daily: 2025-01-07

---
+++ src/main/java/cn/kingen/oj/leetcode/problem/p3000/Solution3019.java
@@ -0,0 +1,30 @@
+package cn.kingen.oj.leetcode.problem.p3000;
+
+import cn.kingen.oj.leetcode.support.Complexity;
+import cn.kingen.oj.leetcode.support.Difficulty;
+import cn.kingen.oj.leetcode.support.Question;
+import cn.kingen.oj.leetcode.support.Tag;
+
+/**
+ * <a href="https://leetcode.cn/problems/number-of-changing-keys/">3019. Number of Changing Keys</a>
+ *
+ * @author Kingen
+ */
+@Question(
+        tags = {Tag.STRING},
+        difficulty = Difficulty.EASY,
+        daily = "2025-01-07"
+)
+public class Solution3019 {
+
+    @Complexity(time = "O(n)", space = "O(1)")
+    public int countKeyChanges(String s) {
+        int n = s.length(), ans = 0;
+        for (int i = 1; i < n; i++) {
+            if ((s.charAt(i) & 31) != (s.charAt(i - 1) & 31)) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
