51619705c8f60877cbe29d82c1d11bac38df02c5
kingen <wsg787@126.com>
2025-01-10 15:50:31 +0800

    daily: 2025-01-10

---
+++ src/main/java/cn/kingen/oj/leetcode/problem/p3200/Solution3298.java
@@ -0,0 +1,26 @@
+package cn.kingen.oj.leetcode.problem.p3200;
+
+import cn.kingen.oj.leetcode.support.Difficulty;
+import cn.kingen.oj.leetcode.support.Question;
+import cn.kingen.oj.leetcode.support.SameAs;
+import cn.kingen.oj.leetcode.support.Tag;
+
+/**
+ * <a href="https://leetcode.cn/problems/count-substrings-that-can-be-rearranged-to-contain-a-string-ii/">3298. Count Substrings That Can Be Rearranged to Contain a String II</a>
+ *
+ * @author Kingen
+ * @see cn.kingen.oj.leetcode.problem.p0.Solution76
+
+ */
+@Question(
+        tags = {Tag.HASH_TABLE, Tag.STRING, Tag.SLIDING_WINDOW},
+        difficulty = Difficulty.HARD,
+        daily = "2025-01-10"
+)
+public class Solution3298 {
+
+    @SameAs(Solution3297.class)
+    public long validSubstringCount(String word1, String word2) {
+        return 0;
+    }
+}
