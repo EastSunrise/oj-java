42be066429ea08aae09c44ba6042d9e0c5228ce3
kingen <wsg787@126.com>
2025-01-17 09:41:33 +0800

    daily: 2025-01-17

---
+++ src/main/java/cn/kingen/oj/leetcode/problem/p3000/Solution3097.java
@@ -0,0 +1,27 @@
+package cn.kingen.oj.leetcode.problem.p3000;
+
+import cn.kingen.oj.leetcode.support.Difficulty;
+import cn.kingen.oj.leetcode.support.Question;
+import cn.kingen.oj.leetcode.support.SameAs;
+import cn.kingen.oj.leetcode.support.Tag;
+
+/**
+ * <a href="https://leetcode.cn/problems/shortest-subarray-with-or-at-least-k-ii/">3097. Shortest Subarray With OR at Least K II</a>
+ *
+ * @author Kingen
+ * @see cn.kingen.oj.leetcode.problem.p300.Solution325
+ * @see cn.kingen.oj.leetcode.problem.p800.Solution862
+
+ */
+@Question(
+        tags = {Tag.BIT_MANIPULATION, Tag.ARRAY, Tag.SLIDING_WINDOW},
+        difficulty = Difficulty.MEDIUM,
+        daily = "2025-01-17"
+)
+public class Solution3097 {
+
+    @SameAs(Solution3095.class)
+    public int minimumSubarrayLength(int[] nums, int k) {
+        return 0;
+    }
+}
